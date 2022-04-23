52be2550ed344e29067c039e98357ed40adc3ea8
skdash505 <skdash505@gmail.com>
2022-04-24 00:02:46 +0530

    Fontaswem replaced by maticon and some other bug fixing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 // Angular material module
 // import { AngularMaterialModule} from './angularMaterial/angular-material.module';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { DialogContentComponent } from './dialog-content/dialog-content.component';
@@ -24,6 +30,7 @@ import { DialogContentComponent } from './dialog-content/dialog-content.componen
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
 
     AppRoutingModule,
 
@@ -31,7 +38,12 @@ import { DialogContentComponent } from './dialog-content/dialog-content.componen
     MatButtonModule,
     MatDialogModule,
     MatTableModule,
-    MatCheckboxModule
+    MatPaginatorModule,
+    MatSortModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/dialog-content/dialog-content.component.ts
+++ src/app/dialog-content/dialog-content.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { AfterViewInit, Component, ViewChild, OnInit, Inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
@@ -11,77 +15,167 @@ import { DataJsonElement } from '../DataJsonElements';
 })
 export class DialogContentComponent implements OnInit {
 
+  // Basic Table
   displayedColumns: string[] = ['selector', 'name', 'description', 'webReference', 'action'];
-  dataList!: DataJsonElement[];  
+  dataListPaginated!: MatTableDataSource<DataJsonElement>;
+  dataList!: DataJsonElement[];
+
+  // highlight selectedRow
   clickedRows = new Set<DataJsonElement>();
-  selectedRows: DataJsonElement[] = [];
   selectAllRow: boolean = false;
+
 
   constructor(
     private dialogRef: MatDialogRef<DialogContentComponent>,
+    private _liveAnnouncer: LiveAnnouncer,
     @Inject(MAT_DIALOG_DATA) data: DataJsonElement[]
   ) {
-    this.dataList = data;
-    this.dataList.forEach(element => {
-      if (!element.selector) {
-        element.selector = false;
-      }
-    });
+    this.setDataList(data);
   }
 
   ngOnInit(): void {
-    console.log(this.dataList);
+    this.consoleAllData("ngOnInit");
   }
 
-  // on click on select all checkbox
-  onSelectAll(event: any) {
-    this.selectAllRow = !this.selectAllRow;
-    this.dataList.map(value => value.selector = !value.selector);
-    this.selectedRows = this.dataList;
-    console.log(this.selectedRows);
-    console.log(this.dataList);
+  ngAfterViewInit() {
+    this.dataListPaginated.sort = this.sort;
+    this.dataListPaginated.paginator = this.paginator;
+    this.consoleAllData("ngAfterViewInit");
   }
 
-  // On click on individual checkbox
-  onSelecter(event: any, row: DataJsonElement) {
-    console.log(row);
-    row.selector = !row.selector;
-    this.dataList[row.id - 1].selector = row.selector;
-    this.selectedRows.push(row);
-    console.log(this.selectedRows);
-    console.log(this.dataList);
+  // pagination Related
+  pageSizeOption = [5, 10, 25, 100];
+  selectedPageSize = this.pageSizeOption[0];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  // Sorting Related
+  @ViewChild(MatSort) sort!: MatSort;
+
+  // Updateing the GidData as per Soring
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
-  
+
+  consoleAllData(From: string) {
+    console.log("Start by : ", From);
+    console.log("selectAllRow : ", this.selectAllRow);
+    console.log("clickedRows : ", this.clickedRows);
+    console.log("dataList : ", this.dataList);
+    console.log("dataListPaginated : ", this.dataListPaginated);
+    console.log("paginator : ", this.paginator);
+    console.log("sort : ", this.sort);
+    console.log("filterValue : ", this.filterValue);
+    console.log("End >>>>");
+  }
+
+  // Set Data to DataList from Parent Screen
+  setDataList(data: DataJsonElement[]) {
+    this.dataList = data;
+    this.dataList.forEach(element => {
+      element.selector = false;
+    });
+    this.updatePaginationDataList(this.dataList);
+    this.consoleAllData("setDataList");
+  }
+
+  // Set Updated Data to Paginated DataList 
+  updatePaginationDataList(updatedDataList: DataJsonElement[]) {
+    this.dataListPaginated = new MatTableDataSource<DataJsonElement>(updatedDataList);
+    this.dataListPaginated.paginator = this.paginator;
+    this.dataListPaginated.sort = this.sort;
+  }
+
   // Add new Row
+  isAddEditRow: boolean = false;
   addRow(event: any) {
-
+    this.consoleAllData("addRow");
   }
 
   // Edit row details
   editRow(event: any, row: DataJsonElement) {
+    this.consoleAllData("editRow");
+  }
 
+
+
+  // on click on select all checkbox
+  onSelectAll(event: any) {
+    this.selectAllRow = !this.selectAllRow;
+    this.dataList.map(value => value.selector = this.selectAllRow);
+    this.consoleAllData("onSelectAll");
+  }
+
+  // On click on individual checkbox
+  onSelecter(event: any, row: DataJsonElement) {
+    row.selector = !row.selector;
+    this.dataList.forEach(index => {
+      if (index.id == row.id) {
+        index.selector = row.selector;
+      }
+    });
+    this.selectAllRow = false;
+    this.consoleAllData("onSelecter");
   }
 
   // Delete Selected Rows
   deleteSelectedRow(event: any) {
-    console.log(event);
-    this.dataList = [];
+
+    let indexList = [];
+    for (let index = 0; index < this.dataList.length; index++) {
+      if (this.dataList[index].selector) {
+        indexList.push(index);
+      }
+    }
+    
+    if (Boolean(indexList.length)) {
+      if (!this.selectAllRow) {
+        indexList.reverse();
+        indexList.forEach(index => {
+          this.dataList.splice(index, 1);
+        });
+      } else {
+        this.dataList = [];
+      }
+      this.updatePaginationDataList(this.dataList);
+    } else {
+      console.warn("no row selected");
+    }
+
+    this.consoleAllData("deleteSelectedRow");
+    if (!Boolean(this.dataList.length)) {
+      this.dialogRef.close();
+    }
   }
 
   // Delete only one row
   deleteRow(event: any, row: DataJsonElement) {
-    console.log(row);
-    console.log(event);
-    for (let toremove = 0; toremove < this.dataList.length; toremove++) {
-      if (row.id == this.dataList[toremove].id) {
-        this.dataList.splice(toremove, 1);
+    for (let index = 0; index < this.dataList.length; index++) {
+      if (row.id == this.dataList[index].id) {
+        this.dataList.splice(index, 1);
       }
     }
-    this.dataList = [...this.dataList];
-    console.log(this.dataList);
+    this.updatePaginationDataList(this.dataList);
+
+    this.consoleAllData("deleteRow");
     if (!Boolean(this.dataList.length)) {
       this.dialogRef.close();
     }
+  }
+
+  // Filter Functionality
+  filterValue!: string;
+  applyFilter(event: any) {
+    // const filterValue = (event.target as HTMLInputElement).value;
+    this.dataListPaginated.filter = this.filterValue.trim().toLowerCase();
+    this.consoleAllData("applyFilter")
+  }
+  clearFilter(event: any) {
+    this.filterValue = "";
+    this.applyFilter(event);
   }
 
 }
